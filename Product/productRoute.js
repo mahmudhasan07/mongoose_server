de9312b258f8fc28179b75f68184feaba804ce1d
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-05-07 02:49:26 +0600

    complete

--- Product/productRoute.js
+++ Product/productRoute.js
@@ -15,17 +15,24 @@ route.get("/items", async (req, res) => {
     res.send(result)
 })
 
-route.get(`/items/:name`, async(req,res)=>{
+route.get(`/items/:name`, async (req, res) => {
     const name = req.params.name
-    const query = {name : {$regex :name, $options : "i" }}
+    const query = { name: { $regex: name, $options: "i" } }
     const result = await productSchema.findOne(query)
     res.send(result)
 })
 
-route.get(`/item/:data`, async(req,res)=>{
+route.get("/itemInfo/:id", async (req, res) => {
+    const id = req.params.id
+    const query = { _id: new Object(id) }
+    const result = await productSchema.findOne(query)
+    res.send(result)
+})
+
+route.get(`/item/:data`, async (req, res) => {
     const sortdata = req.params.data
     console.log(sortdata);
-    const query = {price: 1}
+    const query = { price: 1 }
     const result = await productSchema.find().collation()
     res.send(result)
 })
@@ -36,6 +43,23 @@ route.delete("/items/:id", async (req, res) => {
     const result = await productSchema.deleteOne(query)
     res.send(result)
 
+})
+
+
+route.patch("/updateInfo/:id", async (req, res) => {
+    const id = req.params.id
+    const data = req.body
+    console.log(data);
+    console.log(id);
+    const query = { _id: new Object(id) }
+    const options = { upsert: true }
+    const updateDoc = {
+        $set: {
+            price: data?.price
+        },
+    };
+    const result = await productSchema.updateOne(query, updateDoc, options)
+    res.send(result)
 })
 
 
--- User/userSchema.js
+++ User/userSchema.js
@@ -2,8 +2,8 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    name : String,
-    age : Number,
-    details : String
+    // name : String,
+    // age : Number,
+    // details : String
 })
 module.exports = mongoose.model("users", userSchema)
